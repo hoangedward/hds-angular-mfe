ab8565dfe7ad61be19196e8d7b69751482fe2193
Nguyen Phuoc Nguyen <33418293+fesnguyen@users.noreply.github.com>
2024-01-09 16:48:44 +0700

    Config keycloak

--- hds/apps/shell/src/app/app.module.ts
+++ hds/apps/shell/src/app/app.module.ts
@@ -1,14 +1,39 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakAngularModule } from 'keycloak-angular';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'hds-mfa',
+        clientId: 'hds-mfa-client'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), KeycloakAngularModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ hds/libs/core/src/lib/security/index.ts
@@ -0,0 +1,2 @@
+export * from './guard/auth-guard';
+export * from './interceptors/auth-interceptor';
